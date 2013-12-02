6d2c7188a243d0182608577abc662360fbc5026b
Casyfill <casyfill@gmail.com>
2013-12-02 13:36:53 +0400

    1.102
    тексты как txt, код завернут в функции, читаем все файлы в папке
    
    проблема с кодировкой

--- code/dict1.py
+++ code/dict1.py
@@ -3,77 +3,97 @@
 import os
 from nltk.stem import SnowballStemmer
 import re
+
 stemmer = SnowballStemmer("russian")
 
-filename = '/Users/andy/Dropbox/Projects/Kats/RIA/2013_11_28_President_speech/texts/2000_pu.txt'
-import chardet
-import codecs
+# Определяем имя файлв
+def name(s):
+	n = s.split('/')[-1].split('.')[0] + '.csv'
+	return n
+	pass
 
+# Создаем словарик
+def generateFDict(filename):
+	import chardet
+	import codecs
 
-# Класс словаря
+	# РАСПОЗНАЕМ КОДИРОВКУ
+	bytes = min(32, os.path.getsize(filename))
+	raw = open(filename, 'rb').read(bytes)
 
-
-
-# РАСПОЗНАЕМ КОДИРОВКУ
-bytes = min(32, os.path.getsize(filename))
-raw = open(filename, 'rb').read(bytes)
-
-if raw.startswith(codecs.BOM_UTF8):
-    encoding = 'utf-8-sig'
-else:
-    result = chardet.detect(raw)
-    encoding = result['encoding']
-
-infile = codecs.open(filename, 'r', encoding=encoding)
-txt = infile.read()
-infile.close()
-
-# ПЕРЕВОДИМ В ЮНИКОД
-text = unicode(txt)
-
-
-#СОСТАВЛЯЕМ СЛОВАРЬ РЕГУЛЯРОЧКОЙ
-hello_pattern = re.compile( u'([А-Яа-я]+)', re.UNICODE )
-Wlist = hello_pattern.findall( text )
-
-# ПЕЧАТАЕМ
-# for l in Wlist:
-# 	word = unicode(l).lower()
-# 	print word,'|',stemmer.stem(word)
-
-# СТЕММИМ И СОСТАВЛЯЕМ СЛОВАРИК
-Text_Dict = {}
-
-for w in Wlist:
-	word = unicode(w).lower()
-	wStemmed = stemmer.stem(word)
-	if wStemmed not in Text_Dict.keys():
-		Text_Dict[wStemmed]={}
-		Text_Dict[wStemmed]['dict'] = []
-		Text_Dict[wStemmed]['dict'].append(word)
-		Text_Dict[wStemmed]['count'] = 1
+	if raw.startswith(codecs.BOM_UTF8):
+	    encoding = 'utf-8-sig'
 	else:
-		Text_Dict[wStemmed]['dict'].append(word)
-		Text_Dict[wStemmed]['count'] += 1
+	    result = chardet.detect(raw)
+	    encoding = result['encoding']
+
+	infile = codecs.open(filename, 'r', encoding=encoding)
+	txt = infile.read()
+	infile.close()
+
+	# ПЕРЕВОДИМ В ЮНИКОД
+	text = unicode(txt)
+
+	# СОСТАВЛЯЕМ СЛОВАРЬ РЕГУЛЯРОЧКОЙ
+	hello_pattern = re.compile( u'([А-Яа-я]+)', re.UNICODE )
+	Wlist = hello_pattern.findall( text )
+
+	# ПЕЧАТАЕМ
+	# for l in Wlist:
+	# 	word = unicode(l).lower()
+	# 	print word,'|',stemmer.stem(word)
+
+	# СТЕММИМ И СОСТАВЛЯЕМ СЛОВАРИК
+	Text_Dict = {}
+
+	for w in Wlist:
+		word = unicode(w).lower()
+		wStemmed = stemmer.stem(word)
+		if wStemmed not in Text_Dict.keys():
+			Text_Dict[wStemmed]={}
+			Text_Dict[wStemmed]['dict'] = []
+			Text_Dict[wStemmed]['dict'].append(word)
+			Text_Dict[wStemmed]['count'] = 1
+		else:
+			Text_Dict[wStemmed]['dict'].append(word)
+			Text_Dict[wStemmed]['count'] += 1
 
 
-# РАЧИТЫВАЕМ И ПЕЧАТАЕМ СТАТИСТИУ
+	# РАЧИТЫВАЕМ И ПЕЧАТАЕМ СТАТИСТИУ
 
-resList = []
-for key in Text_Dict.keys():
-	# print key , ':', (', ').join(Text_Dict[key])
-	rString =  key + '|' + str(Text_Dict[key]['count'])  + '|' + (',').join(Text_Dict[key]['dict'])
-	print rString
-	rString+='\n'
-	resList.append(rString.encode('utf-8'))
+	resList = []
+	for key in Text_Dict.keys():
+		# print key , ':', (', ').join(Text_Dict[key])
+		rString =  key + '|' + str(Text_Dict[key]['count'])  + '|' + (',').join(Text_Dict[key]['dict'])
+		# print rString
+	# 	rString+='\n'
+		resList.append(rString.encode('utf-8'))
 
-resList[-1].replace('\n','')
-# print resList
+	# resList[-1].replace('\n','')
+	return resList
+	pass
 
-workfile ='/Users/andy/Dropbox/Projects/Kats/RIA/2013_11_27_Speeches/Dicts/2000_pu.csv'
-with open('workfile', 'w') as f:
+
+# создаем список файлов в папке
+basePath = "/Users/andy/Dropbox/Projects/Kats/RIA/2013_11_28_President_speech/texts/"
+os.chdir(basePath)
+for files in os.listdir("."):
+	filename = basePath + files
+	print files
+
+	# для каждого файлв узнаем имя и создае словарь
+	n = name(filename)
+
+	result = generateFDict(filename)
+
+	#  для каждого файла сохраняем словарик
+	workfile ='/Users/andy/Dropbox/Projects/Kats/RIA/2013_11_28_President_speech/Dicts/' + n
+
+	f = open(workfile, 'w')
 	write_data = f.writelines(resList)
-f.closed
-print 'done'
+	f.closed
+	print n, ' done'
+
+print 'all functions are done'
 
 
